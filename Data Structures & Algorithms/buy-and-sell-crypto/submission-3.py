746c22d7669a87a18ffe614fbfb6130c8f448367
ShreyAnalyses <155176013+ShreyAnalyses@users.noreply.github.com>
2026-05-11 17:32:42 +0530

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.py
@@ -0,0 +1,47 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # ta = False # transaction availble status, can not purchase again (before sellling) if have already purchased
+
+        result = 0
+
+        for b in range(len(prices)):
+            buy = prices[b]
+            for s in range(b+1, len(prices)):
+                sell = prices[s]
+
+                result = (sell - buy) if (sell - buy) >  result else result
+
+        return result
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+
+        mp = prices[0]
+        profit = 0
+
+        for i in range(1, len(prices)):
+            mp = prices[i] if prices[i] < mp else mp
+
+            profit = prices[i] - mp if prices[i] - mp > profit else profit
+
+        return profit
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        l,r = 0,1
+        maxP = 0
+
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                profit = prices[r] - prices[l]
+                maxP = max(maxP, profit)
+            else:
+                l=r
+
+            r += 1
+
+        return maxP
+
+
